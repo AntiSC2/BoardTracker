fed1fd7fdbd22b4a1239266e4be1f00a8b6655b8
Jakob Sinclair <sinclair.jakob@openmailbox.org>
2016-04-24 00:09:37 +0200

    board_tracker: setting up the window
    Signed-off-by: Jakob Sinclair <sinclair.jakob@openmailbox.org>

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,51 @@
  * Authors: Jakob Sinclair <sinclair.jakob@openmailbox.org>
  */
 
+#[macro_use] extern crate conrod;
+extern crate piston_window;
 extern crate bt_lib;
 
+use conrod::{
+    color,
+    Button,
+    Canvas,
+    Circle,
+    Color,
+    Colorable,
+    DropDownList,
+    EnvelopeEditor,
+    Frameable,
+    Labelable,
+    NumberDialer,
+    Point,
+    Positionable,
+    Slider,
+    Sizeable,
+    Text,
+    TextBox,
+    Theme,
+    Toggle,
+    Widget,
+    WidgetMatrix,
+    XYPad,
+};
+use piston_window::{EventLoop, Glyphs, PistonWindow, UpdateEvent, 
+                    WindowSettings};
 use bt_lib::entity::player::*;
 
+type Backend = (<piston_window::G2d<'static> as conrod::Graphics>::Texture, Glyphs);
+type Ui = conrod::Ui<Backend>;
+type UiCell<'a> = conrod::UiCell<'a, Backend>;
+
 fn main() {
+    let mut window: PistonWindow =
+        WindowSettings::new("Board Tracker", [1280, 720])
+            .exit_on_esc(true).vsync(true).samples(4).build().unwrap();
+
+    window.set_ups(60);
     let player1 = Player::new(0);
+    while let Some(event) = window.next() {
+        
+    }
     println!("Hello world!");
 }
